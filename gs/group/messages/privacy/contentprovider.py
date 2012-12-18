c83f48e5eb36e177675e87d6c8c0f25d8ad9cde6
Michael JasonSmith <mpj17@onlinegroups.net>
2012-12-19 10:42:10 +1100

    Moving the post privacy content provider here, from Products.XWFMailingListManager

---
+++ gs/group/messages/privacy/contentprovider.py
@@ -0,0 +1,53 @@
+# coding=utf-8
+from AccessControl.PermissionRole import rolesForPermissionOn
+from zope.pagetemplate.pagetemplatefile import PageTemplateFile
+from zope.interface import implements, Interface
+from zope.component import createObject, adapts, provideAdapter
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.contentprovider.interfaces import IContentProvider, UpdateNotCalled
+from gs.group.base.contentprovider import GroupContentProvider
+
+
+class GSPostPrivacyContentProvider(GroupContentProvider):
+    """GroupServer Post Message Content Provider
+    """
+
+    def __init__(self, context, request, view):
+        super(GSPostPrivacyContentProvider, self).__init__(context, request, 
+                                                           view)
+        self.__updated = False
+
+    def update(self):
+        roles = rolesForPermissionOn('View', self.context)
+        anonView = 'Anonymous' in roles
+          
+        self.visibility = anonView and u'public' or u'private'
+        
+        if anonView:
+            self.webVisibility = u'<strong>Anyone</strong> &#8213; '\
+              u'including search engines and people who are not '\
+              u' logged in &#8213;'
+            self.emailVisibility = u'<strong>group</strong> members' 
+        elif 'DivisionMember' in roles:
+            self.webVisibility = u'<strong>Group</strong> members '\
+              u'and <strong>site</strong> members'
+            self.emailVisibility = u'<strong>group</strong> members' 
+        else:
+            self.webVisibility = u'<strong>Group</strong> members'
+            self.emailVisibility = u'' 
+        
+        assert type(self.visibility) == unicode
+        assert type(self.webVisibility) == unicode
+        assert type(self.emailVisibility) == unicode
+        self.__updated = True
+            
+    def render(self):
+        if not self.__updated:
+            raise UpdateNotCalled
+        pageTemplate = PageTemplateFile(self.pageTemplateFileName)
+        retval = pageTemplate(
+                visibility =      self.visibility,
+                webVisibility =   self.webVisibility,
+                emailVisibility = self.emailVisibility)
+        assert type(retval) == unicode
+        return retval
