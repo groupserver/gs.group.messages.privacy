115493472b5d395b26ee2f3d84535c1eabb12802
Michael JasonSmith <mpj17@onlinegroups.net>
2014-02-06 13:32:45 -0800

    Adding a Copyright block, switching to Unicode literals, and absolute import.

--- gs/group/messages/privacy/contentprovider.py
+++ gs/group/messages/privacy/contentprovider.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import absolute_import, unicode_literals
 from zope.contentprovider.interfaces import UpdateNotCalled
 from zope.pagetemplate.pagetemplatefile import PageTemplateFile
 from gs.group.base import GroupContentProvider
-from privacy import MessagesPrivacy
+from .privacy import MessagesPrivacy
 
 
 class GSPostPrivacyContentProvider(GroupContentProvider):
@@ -16,21 +30,18 @@ class GSPostPrivacyContentProvider(GroupContentProvider):
         self.visibility = messagesPrivacy.visibility
 
         if messagesPrivacy .anon:
-            self.webVisibility = u'<strong>Anyone</strong> &#8213; '\
-              u'including search engines and people who are not '\
-              u' logged in &#8213;'
-            self.emailVisibility = u'<strong>group</strong> members'
+            self.webVisibility = '<strong>Anyone</strong> &#8213; '\
+              'including search engines and people who are not '\
+              'logged in &#8213;'
+            self.emailVisibility = '<strong>group</strong> members'
         elif messagesPrivacy .site:
-            self.webVisibility = u'<strong>Group</strong> members '\
-              u'and <strong>site</strong> members'
-            self.emailVisibility = u'<strong>group</strong> members'
+            self.webVisibility = '<strong>Group</strong> members '\
+              'and <strong>site</strong> members'
+            self.emailVisibility = '<strong>group</strong> members'
         else:
-            self.webVisibility = u'<strong>Group</strong> members'
-            self.emailVisibility = u''
+            self.webVisibility = '<strong>Group</strong> members'
+            self.emailVisibility = ''
 
-        assert type(self.visibility) == unicode
-        assert type(self.webVisibility) == unicode
-        assert type(self.emailVisibility) == unicode
         self.__updated = True
 
     def render(self):
@@ -40,5 +51,4 @@ class GSPostPrivacyContentProvider(GroupContentProvider):
         retval = pageTemplate(visibility=self.visibility,
                                 webVisibility=self.webVisibility,
                                 emailVisibility=self.emailVisibility)
-        assert type(retval) == unicode
         return retval
--- gs/group/messages/privacy/interfaces.py
+++ gs/group/messages/privacy/interfaces.py
@@ -1,11 +1,24 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import unicode_literals
 from zope.interface import Interface
 from zope.schema import ASCIILine
 
 
 class IGSPostPrivacyContentProvider(Interface):
-    pageTemplateFileName = ASCIILine(title=u"Page Template File Name",
-        description=u"""The name of the ZPT file
-        that is used to render the post.""",
+    pageTemplateFileName = ASCIILine(title="Page Template File Name",
+        description="The name of the ZPT file that is used to render the post.",
         required=False,
-        default="browser/templates/dialog.pt")
+        default="browser/templates/dialog.pt".encode('ascii', 'ignore'))
