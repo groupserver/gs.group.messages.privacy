54fec49def25242d58c37414d72f18a3cda22d1b
Michael JasonSmith <mpj17@onlinegroups.net>
2014-11-12 11:23:54 -0800

    Fixing the declaration of the message factory (issues with a circular import)

--- gs/group/messages/privacy/__init__.py
+++ gs/group/messages/privacy/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+from zope.i18nmessageid import MessageFactory
+GSMessageFactory = MessageFactory('gs.group.messages.privacy')
 #lint:disable
 from .privacy import MessagesPrivacy
 #lint:enable
-
-from zope.i18nmessageid import MessageFactory
-GSMessageFactory = MessageFactory('gs.group.messages.privacy')
